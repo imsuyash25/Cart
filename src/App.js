533f018052e3575f4f3a070bf003b404d7ccea91
imsuyash25 <asatisuyash@gmail.com>
2022-06-25 19:18:36 +0530

    react app repo  created with first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+
+
+function App(){
+  
+  return (
+    <div className="App">
+    <h1>hello world</h1>
+    <div className="flex">
+      <strong>Hello world</strong>
+      </div>
+    </div>
+  );
+}
+export default App;
